557ac2599f716cdbc1b0e0fc6fb277c6410f31f2
Alex Golesh <alex.golesh@hotmail.com>
2020-07-27 11:20:42 -0700

    Updated API endpoint

--- github-experts-ui/src/layout/VideoCall.js
+++ github-experts-ui/src/layout/VideoCall.js
@@ -3,15 +3,12 @@ import { useParams } from 'react-router-dom';
 import Loading from 'layout/Loading';
 
 function sessionDone(roomName) {
-  fetch(`${process.env.REACT_APP_API_ROOT}/calldone`, {
+  fetch(`${process.env.REACT_APP_API_ROOT}/calldone/${roomName}`, {
     method: 'POST',
     headers: {
       // TODO: once auth is integrated into the frontend, add Authorization header here to pass-through B2C Bearer token
       'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      roomName,
-    }),
+    }
   })
     .then((response) => {
       if (response.status !== 200) {
