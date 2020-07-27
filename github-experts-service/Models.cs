d99f5cf615904f716aa995f610758833488d036f
Clemens Wolff <clewolff@microsoft.com>
2020-07-27 17:12:54 -0400

    Wire up authenticated API calls in frontend (#56)

--- github-experts-service/Models.cs
+++ github-experts-service/Models.cs
@@ -28,5 +28,8 @@ namespace GithubExpertsService
     {
         [JsonPropertyName("name")]
         public string Name { get; set; }
+
+        [JsonPropertyName("avatar_url")]
+        public string AvatarUrl { get; set; }
     }
 }
--- github-experts-ui/src/layout/Home.js
+++ github-experts-ui/src/layout/Home.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import Loading from 'layout/Loading';
+import * as userStore from 'stores/userStore';
 
 export default function Home() {
-  return <span>Hello world!</span>;
+  const user = useSelector(userStore.get);
+
+  if (!user?.name) {
+    return <Loading />;
+  }
+
+  return (
+    <>
+      <div>Hello {user.name}</div>
+      <img src={user.avatarUrl} alt={`${user.name}'s avatar`} />
+    </>
+  );
 }
