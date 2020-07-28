82332919a7057ae0469686387cafb7627ca82125
Jeong Lim <jlim8612@gmail.com>
2020-07-27 17:50:30 -0700

    Add scheduler (#59)
    * Add scheduler
    
    * Change component name

--- github-experts-ui/src/layout/App.js
+++ github-experts-ui/src/layout/App.js
@@ -3,6 +3,7 @@ import { HashRouter, Route, Switch } from 'react-router-dom';
 import Loading from 'layout/Loading';
 import { SchedulePage } from '../pages/schedule';
 import PrivateRoute from 'components/PrivateRoute';
+import { SchedulerPage } from 'pages/scheduler';
 
 const VideoCall = lazy(() => import('layout/VideoCall'));
 const Home = lazy(() => import('layout/Home'));
@@ -14,6 +15,7 @@ export default function App() {
       <Suspense fallback={<Loading />}>
         <Switch>
           <Route exact path="/schedule" component={SchedulePage} />
+          <Route exact path="/scheduler" component={SchedulerPage} />
           <PrivateRoute exact path="/" component={Home} />
           <Route exact path="/videocall/:roomName" component={VideoCall} />
           <Route exact path="/id_token=:idToken" component={IdToken} />
