6a012a23f239eb68402c5042981688c0c01ad988
William Kilday <williamkilday@williams-mbp.lan>
2020-07-27 23:35:35 -0700

    adding UI for picking a coaching session

---
+++ github-experts-ui/src/components/ToggleButton/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const ToggleButton = styled(({ className, value, selected, disabled, children, classes }) => {
+
+  var btnClass = selected ? 'btn-selected' : '';
+  var isDisabled = disabled ? 'disabled' : '';
+
+  return (
+    <button type="button" className={`${className} ${classes} ${btnClass} btn mr-2 mb-2`} disabled={isDisabled}>{children}</button>
+  )
+})`
+  & {
+    color: #1999DF;
+    background: #FAFBFC;
+    border: 1px solid #1999DF;
+    box-sizing: border-box;
+    box-shadow: 0px 1px 0px rgba(27, 31, 35, 0.04), inset 0px 2px 0px rgba(255, 255, 255, 0.25);
+    border-radius: 6px;
+  }
+  &.btn-selected {
+    color: #FFFFFF;
+    background: #1999DF;
+    border: 1px solid #1999DF;
+    box-sizing: border-box;
+    box-shadow: 0px 1px 0px rgba(27, 31, 35, 0.04), inset 0px 2px 0px rgba(255, 255, 255, 0.25);
+    border-radius: 6px;
+  }
+`;
--- github-experts-ui/src/pages/schedule/index.js
+++ github-experts-ui/src/pages/schedule/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { BoxPanel } from 'components/BoxPanel';
 import { Layout } from 'components/layout';
+import { SessionPicker } from 'pages/schedule/components/SessionPicker';
+import { BreadCrumbs } from 'components/Breadcrumbs';
 
 export const SchedulePage = () => (
   <Layout headerOptions={[{ text: 'Schedule', path: '/schedule' }]}>
-    <BoxPanel>insert component here</BoxPanel>
+    <BoxPanel>
+      <BreadCrumbs breadCrumbPaths={['patniko', 'experts-demo', 'Schedule a coaching session']} />
+      <SessionPicker></SessionPicker>
+    </BoxPanel>
   </Layout>
 );
