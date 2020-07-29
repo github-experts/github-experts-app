472ed06a83198f33e6f28e17e773c1d89d6e66d4
Clemens Wolff <clewolff@microsoft.com>
2020-07-29 18:16:22 -0400

    Add devlogin to simulate production EasyAuth

--- github-experts-ui/src/layout/App.js
+++ github-experts-ui/src/layout/App.js
@@ -1,5 +1,10 @@
 import React, { Suspense, lazy } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
 import Loading from 'layout/Loading';
 import { SchedulerPage } from 'pages/scheduler';
 import { ScheduleSummary } from 'pages/schedule-summary';
@@ -10,6 +15,26 @@ import {
 } from 'pages/schedule-request-form';
 
 const VideoCall = lazy(() => import('layout/VideoCall'));
+
+// this is just to enable local development with B2C authentication
+// in production this route isn't used as auth is handled by AppService EasyAuth
+function DevLogin() {
+  const prefix = '#id_token=';
+  const idToken = window.location.hash.startsWith(prefix)
+    ? window.location.hash.substring(prefix.length)
+    : null;
+
+  if (!idToken) {
+    return (
+      <a href="https://h3dev.b2clogin.com/h3dev.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_github&client_id=144e9bd1-f04e-4454-abc6-1dae3af35cc4&nonce=defaultNonce&redirect_uri=http%3A%2F%2Flocalhost%3A3000/devlogin&scope=openid&response_type=id_token&prompt=login">
+        Click here for dev login
+      </a>
+    );
+  } else {
+    localStorage.setItem('id_token', idToken);
+    return <Redirect to="/" />;
+  }
+}
 
 export default function App() {
   return (
@@ -34,6 +59,7 @@ export default function App() {
             component={ScheduleRequestFormSuccess}
           />
           <Route exact path="/videocall/:roomName" component={VideoCall} />
+          <Route exact path="/devlogin" component={DevLogin} />
         </Switch>
       </Suspense>
     </Router>
---
+++ github-experts-ui/src/utils/request.js
@@ -0,0 +1,15 @@
+const apiRoot = process.env.REACT_APP_API_ROOT || '/api';
+
+export default function request(url, opts) {
+  const token = localStorage.getItem('id_token');
+
+  if (token) {
+    // means we're running on localhost, in production we're using the EasyAuth cookie
+    // so no need to pass the authorization header there
+    const headers = opts.headers || {};
+    headers.Authorization = `Bearer ${token}`;
+    opts.headers = headers;
+  }
+
+  return fetch(`${apiRoot}/${url}`, opts);
+}
