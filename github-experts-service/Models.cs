f9daef9e1ea6e7385d4dbdb77c94c6a160e14171
Steve Hall <sthall@microsoft.com>
2020-07-29 13:50:51 -0400

    Fixed monthly availability

--- github-experts-service/Models.cs
+++ github-experts-service/Models.cs
@@ -33,39 +33,6 @@ namespace GithubExperts.Api
         public string AvatarUrl { get; set; }
     }
 
-    public class DailyAvailability
-    {
-        [JsonPropertyName("date")]
-        public string Date { get; set; }
-
-        [JsonPropertyName("starttime")]
-        public string StartTime { get; set; }
-
-        // bitmap is a string that corresponds to each 30 minute segment
-        // for an Expert beginning with their start time. A bitmap corresponding
-        // to an Expert available from 9am to 11am, with a 10:30 appointment
-        // booked would look like "AAAB" (9:00 available, 9:30 available,
-        // 10:00 available, 10:30 booked)
-        [JsonPropertyName("bitmap")]
-        public string Bitmap { get; set; }
-    }
-
-    public class MonthlyAvailability
-    {
-        [JsonPropertyName("date")]
-        public string Date { get; set; }
-
-        // bitmap is a string that corresponds to each day of the month,
-        // beginning with the date property. Days that have availability are
-        // marked with 'O' (oh), days without availability, or not taking
-        // appointments are marked with a '-' (dash). Example:
-        // "--OOOOO--O-OOO--OOOOO--OOOOO--OOOOO--O"
-        // For a period beginning 08/01/2020, the first two days of Aug have
-        // no availability, but 8/3 - 8/7 all have availability.
-        [JsonPropertyName("bitmap")]
-        public string Bitmap { get; set; }
-    }
-
     public class RequestError
     {
         [JsonPropertyName("message")]
