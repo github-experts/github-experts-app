8ca6b4705dc9bcb10b496e56c52a95664fb632be
Clemens Wolff <clewolff@microsoft.com>
2020-07-30 16:02:15 -0400

    Add links to frontend to email (#96)

--- github-experts-service/Util/EmailUtil.cs
+++ github-experts-service/Util/EmailUtil.cs
@@ -23,15 +23,18 @@
         {
             var template = string.Empty;
             var toAddr = string.Empty;
+            var link = string.Empty;
             if (appointment.Status == "requested")
             {
                 template = "request";
                 toAddr = expert.Email;
+                link = $"https://{Environment.GetEnvironmentVariable("BACKEND_HOSTNAME")}/site/scheduler/{expert.Handle}";
             }
             else if (appointment.Status == "accepted")
             {
                 template = "confirmation";
                 toAddr = appointment.Requestor;
+                link = $"https://{Environment.GetEnvironmentVariable("BACKEND_HOSTNAME")}/site/videocall/{appointment.Id}";
             }
 
             /* TODO template conversions for 'completed' and 'rejected' */
@@ -41,6 +44,7 @@
             placeholders.Add("date", appointment.DateTime.ToString("dddd, MMMM dd yyyy"));
             placeholders.Add("time", appointment.DateTime.ToString("h:mm tt"));
             placeholders.Add("repo name here", appointment.Repo);
+            placeholders.Add("link", link);
             return new EmailMessage
             {
                 To = toAddr,
